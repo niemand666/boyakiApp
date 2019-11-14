48b4d9420a3df8ee183695b093817ec848d34716
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-11-14 19:04:08 +0900

    投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,58 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all
+  end
+
+
+  def new
+    @post = Post.new
+  end
+
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+
+  def update
+    @post = Post.find(params[:id])
+  
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  
+    redirect_to posts_path
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:content)
   end
 end
