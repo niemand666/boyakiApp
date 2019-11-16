0e347214ee6400e7242417496f64a040418d7674
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-11-16 19:30:07 +0900

    時刻表示を日本にする

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find_by(id: @post.user_id)
     @comments = @post.comments.includes(:user)
+    @comments_count = Comment.where(post_id: @post.id).count
   end
 
 
