5092d4a8022c41c54f627d601c2c69fb7f5d0cc0
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-11-20 15:26:17 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,13 +36,11 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload secrets.yml'
+  desc 'upload master.key'
   task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    on roles(:app) do |_host|
+      execute "mkdir -p #{shared_path}/config" if test "[ ! -d #{shared_path}/config ]"
+      # upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
   before :starting, 'deploy:upload'
