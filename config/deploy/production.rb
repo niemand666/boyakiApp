5935c41fc217c77b70137c55156cc18fcad80a5c
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-11-20 13:30:51 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '52.192.191.134', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -59,4 +60,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.192.191.134', user: 'ec2-user', roles: %w{app db web}
