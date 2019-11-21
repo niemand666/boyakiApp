0f4e89d268b91ff7495982a44b110db4e9e3634d
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-11-21 20:19:54 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,11 +30,12 @@ set :linked_files, %w[config/master.key]
 # set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
-#after 'deploy:publishing', 'deploy:restart'
-#namespace :deploy do
-#  task :restart do
-#    invoke 'unicorn:restart'
-#  end
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
 
 #  desc 'upload master.key'
 #  task :upload do
