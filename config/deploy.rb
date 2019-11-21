a1884b48893507d3f20cda2c4aa2dfa7812b4f7e
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-11-21 20:03:21 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,8 +33,7 @@ set :linked_files, %w[config/master.key]
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
+    invoke 'unicorn:restart'
   end
 
   desc 'upload master.key'
