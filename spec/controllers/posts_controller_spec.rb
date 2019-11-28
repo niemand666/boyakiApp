cbd94e8f7fcd6c1d602b4dea0b36ca7b5ce266f1
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-11-29 02:08:52 +0900

    Update posts_controller_spec.rb

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe PostsController do
-
   describe 'GET #new' do
     it "renders the :new template" do
       get :new
@@ -24,10 +23,15 @@ describe PostsController do
   end
 
   describe 'GET #index' do
+    it "populates an array of posts ordered by created_at DESC" do
+      posts = create_list(:post, 3) 
+      get :index
+      expect(assigns(:posts)).to match(posts.sort{ |a, b| b.created_at <=> a.created_at } )
+    end
+
     it "renders the :index template" do
       get :index
       expect(response).to render_template :index
     end
   end
-
 end
