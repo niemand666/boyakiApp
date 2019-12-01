172c0c796bec38aa495f2fd2907357e57954760e
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-12-02 00:54:35 +0900

    マイグレーションファイルの修正

--- db/migrate/20191115081427_create_comments.rb
+++ db/migrate/20191115081427_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :post_id
-      t.text :text
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
+      t.text :text, null: false
       t.timestamps
     end
   end
