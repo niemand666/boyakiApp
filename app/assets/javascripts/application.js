82f5896a36b376f9b3a25f3de9607a00d93dd85c
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-12-01 16:49:09 +0900

    create-icon.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,17 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
-$(document).on('turbolinks:load', function() {
-  function readURL(input) {
-      if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        $('#img_prev').attr('src', e.target.result);
-      }
-      reader.readAsDataURL(input.files[0]);
-    }}
-    $("#user_img").change(function(){
-      readURL(this);
-    });
-  });
