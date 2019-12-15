dde2232b6016bc8ecd5709516af10b580f4a2f06
niemand666 <in6ca6on6vo6ti@gmail.com>
2019-12-15 19:29:57 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,10 @@
 server '52.192.191.134', user: 'ec2-user', roles: %w{app db web}
 
-# set :ssh_options, {
-#     forward_agent: true,
-#     user: fetch(:user),
-#     keys: ["#{ENV['SERVICE_NAME_PRODUCTION_SSH_KEY']}"]
-# }
+set :ssh_options, {
+    forward_agent: true,
+    user: fetch(:user),
+    keys: ["#{ENV['SERVICE_NAME_PRODUCTION_SSH_KEY']}"]
+}
 
 # server-based syntax
 # ======================
